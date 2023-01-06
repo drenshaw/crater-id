9bf2f3872eb12031cfe0add3729204e275988257
dqr0509 <drenshaw@draper.com>
2023-01-06 13:50:23 -0600

    Moved things over to the header files

---
+++ include/crater-id.h
@@ -0,0 +1,67 @@
+#include <string>
+#include <vector>
+
+enum columns {
+    CRATER_ID,
+    LAT_CIRC_IMG,
+    LON_CIRC_IMG,
+    LAT_ELLI_IMG,
+    LON_ELLI_IMG,
+    DIAM_CIRC_IMG,
+    DIAM_CIRC_SD_IMG,
+    DIAM_ELLI_MAJOR_IMG,
+    DIAM_ELLI_MINOR_IMG,
+    DIAM_ELLI_ECCEN_IMG,
+    DIAM_ELLI_ELLIP_IMG,
+    DIAM_ELLI_ANGLE_IMG,
+    LAT_ELLI_SD_IMG,
+    LON_ELLI_SD_IMG,
+    DIAM_ELLI_MAJOR_SD_IMG,
+    DIAM_ELLI_MINOR_SD_IMG,
+    DIAM_ELLI_ANGLE_SD_IMG,
+    DIAM_ELLI_ECCEN_SD_IMG,
+    DIAM_ELLI_ELLIP_SD_IMG,
+    ARC_IMG,
+    PTS_RIM_IMG
+};
+
+struct martian_crater {
+    std::string crater_id;
+	float  lat;
+	float  lon;
+	float  diam;
+    float  ecc;
+    uint   npts;
+    std::string crater_name;
+};
+
+struct lunar_crater {
+    std::string crater_id;
+	float  lat;
+	float  lon;
+	float  diam;
+	float  ecc;
+	float  ell;
+	float  arc;
+	uint   npts;
+};
+
+bool readLunarCraterEntry(  const std::string, 
+                            lunar_crater&,
+                            const char, 
+                            const float = 1.2, 
+                            const float = 0.9,
+                            const float = 60.0,
+                            const float = 1000.0);
+
+std::vector<lunar_crater> runCraterReader(const std::string,
+                                     const char =',',
+                                     const float =1.2,
+                                     const float =0.9,
+                                     const uint =1e6);
+
+std::tuple<float, char> reinterpret_lat(const float);
+std::tuple<float, char> reinterpret_lon(const float);
+std::tuple<std::string, std::string> reinterpret_latlon(const float, const float);
+void printLunarCratersInfo(const lunar_crater);
+bool readLunarCraterEntry(const std::string, const char);
--- src/main.cpp
+++ src/main.cpp
@@ -3,124 +3,189 @@
 #include <string>
 #include <vector>
 #include <sstream>
-// #include "include/crater-id.h"
+#include <math.h>
+#include <tuple>
+#include "crater-id.h"
  
 using namespace std;
-
-struct martian_crater {
-    string CRATER_ID;
-    float  LATITUDE_CIRCLE_IMAGE;
-    float  LONGITUDE_CIRCLE_IMAGE;
-    float  DIAM_CIRCLE_IMAGE;
-    float  DIAM_ELLIPSE_ECCEN_IMAGE;
-    uint    PTS_USED_RIM_IMAGE;
-    string CRATER_NAME;
-};
-
-struct lunar_crater {
-    string CRATER_ID;
-	float  LAT_CIRC_IMG;
-	float  LON_CIRC_IMG;
-	float  DIAM_CIRC_IMG;
-	float  DIAM_CIRC_SD_IMG;
-	float  DIAM_ELLI_ECCEN_IMG;
-	float  DIAM_ELLI_ELLIP_IMG;
-	float  ARC_IMG;
-	uint   PTS_RIM_IMG;
-};
-
-bool readMartianCraterEntry(const string entry, const char sep) {
-    martian_crater crater;
-    string lat, lon, diam, ecc, n_pts;
-    stringstream str(entry);
-    getline(str, crater.CRATER_ID, sep);
-    getline(str, lat, sep);
-    getline(str, lon, sep);
-    getline(str, diam, sep);
-    getline(str, ecc,  sep);
-    getline(str, n_pts,sep);
-    getline(str, crater.CRATER_NAME, sep);
-    try {
-        crater.LATITUDE_CIRCLE_IMAGE = stof(lat);
-        crater.LONGITUDE_CIRCLE_IMAGE = stof(lon);
-        crater.DIAM_CIRCLE_IMAGE = stof(diam);
-        crater.DIAM_ELLIPSE_ECCEN_IMAGE = stof(ecc);
-        crater.PTS_USED_RIM_IMAGE = stof(n_pts);
-    } catch (const exception& e) {return false;}
-    return crater.DIAM_ELLIPSE_ECCEN_IMAGE < 0.3;
-}
-
-bool readLunarCraterEntry(const string entry) {
-    return false;
-}
  
 int main() {
     string fname;
     // cout<<"Enter crater file name: ";
     // cin>>fname;
-    fname = "/home/ndvr/Documents/Research/CraterDatabase/crater_reduced.csv";
-    
-    martian_crater db_entry;
-    vector<vector<string>> content;
-    vector<string> row;
-    vector<martian_crater> entries;
-    string line, word;
-    // string crater_id, crater_name;
-    string lat, lon, diam, ecc, n_pts;
+    // fname = "/home/ndvr/Documents/Research/CraterDatabase/crater_reduced.csv";
+    fname = "/home/dqr0509/data/craters/lunar_craters.csv";
 
-    // /home/ndvr/Documents/Research/CraterDatabase/crater_reduced.csv
-    char sep = ',';
-    // char cout_sep = '\t';
+    // // /home/ndvr/Documents/Research/CraterDatabase/crater_reduced.csv
+
+    vector<lunar_crater> craters = runCraterReader(fname);
     
+    return 0;
+}
+
+bool readLunarCraterEntry(  const string entry, 
+                            lunar_crater& crater,
+                            const char sep, 
+                            const float max_ell,
+                            const float min_arc,
+                            const float min_diam,
+                            const float max_diam) {
+    // lunar_crater crater;
+    string lat, lon, diam, ecc, n_pts;
+    stringstream str(entry);
+    string token;
+    uint count = 0;
+    while(getline(str, token, sep)) {
+        if(token.empty())
+            return false;
+        switch(count) {
+            case CRATER_ID:
+                crater.crater_id = token;
+                break;
+            case LAT_CIRC_IMG:
+                crater.lat = stof(token);
+                break;
+            case LON_CIRC_IMG:
+                crater.lon = stof(token);
+                break;
+            case DIAM_CIRC_IMG:
+                crater.diam = stof(token);
+                break;
+            case DIAM_ELLI_ECCEN_IMG:
+                crater.ecc = stof(token);
+                break;
+            case DIAM_ELLI_ELLIP_IMG:
+                crater.ell = stof(token);
+                break;
+            case ARC_IMG:
+                crater.arc = stof(token);
+                break;
+            default:
+                break;
+        }
+        count++;
+    }
+    return crater.ell < max_ell 
+        && crater.arc > min_arc
+        && crater.diam < max_diam
+        && crater.diam > min_diam;
+}
+
+vector<lunar_crater> runCraterReader(const string fname,
+                                     const char sep,
+                                     const float max_ell,
+                                     const float min_arc,
+                                     const uint max_n_craters) {
+    // martian_crater db_entry;
+    lunar_crater   db_entry;
+    vector<lunar_crater> entries;
+    string line;
+
     ifstream file (fname, ios::in);
     if(file.is_open()) {
         uint count = 0;
+
         while(getline(file, line)) {
-            row.clear();
-            count++;
             stringstream str(line);
-            if(count<2) {
-                cout<<line<<endl;
-                while(getline(str, word, sep))
-                    row.push_back(word);
+            if(count==0) {
+                count++;
+                // readLunarCraterEntry(line, sep);
                 continue;
             }
-            getline(str, db_entry.CRATER_ID, sep);
-            getline(str, lat, sep);
-            getline(str, lon, sep);
-            getline(str, diam, sep);
-            getline(str, ecc,  sep);
-            getline(str, n_pts,sep);
-            getline(str, db_entry.CRATER_NAME, sep);
-            try {
-            db_entry.LATITUDE_CIRCLE_IMAGE = stof(lat);
-            db_entry.LONGITUDE_CIRCLE_IMAGE = stof(lon);
-            db_entry.DIAM_CIRCLE_IMAGE = stof(diam);
-            db_entry.DIAM_ELLIPSE_ECCEN_IMAGE = stof(ecc);
-            db_entry.PTS_USED_RIM_IMAGE = stof(n_pts);
-            } catch (const exception& e) {continue;}
-            if(db_entry.DIAM_ELLIPSE_ECCEN_IMAGE > 0.9)
+            if(readLunarCraterEntry(line, db_entry, sep, max_ell, min_arc)) {
+                count++;
                 entries.push_back(db_entry);
-            // cout << db_entry.CRATER_ID << cout_sep 
-            //      << db_entry.LATITUDE_CIRCLE_IMAGE << cout_sep 
-            //      << db_entry.LONGITUDE_CIRCLE_IMAGE << cout_sep 
-            //      << db_entry.DIAM_CIRCLE_IMAGE << cout_sep 
-            //      << db_entry.DIAM_ELLIPSE_ECCEN_IMAGE << cout_sep 
-            //      << db_entry.PTS_USED_RIM_IMAGE << cout_sep 
-            //      << db_entry.CRATER_NAME << endl;
+            }
+            if(count > max_n_craters) {
+                break;
+            }
         }
+        file.close();
     }
     else
         cout<<"Could not open the file: "<<fname<<endl;
+
+    cout<<"Database contains "<<entries.size()<<" entries"<<endl<<endl;
+    
     uint count = 0;
-    for (const auto& cline : content) {
-        for (const auto& data : cline) {
-            cout<<data<<"  \t";
-        }
+    uint n_print = 6;
+    for (const auto& crater : entries) {
+        printLunarCratersInfo(crater);
         cout<<endl;
-        if(count++> 10)
+        if(++count >= n_print)
             break;
     }
-    
-    return 0;
+    return entries;
+}
+
+bool readMartianCraterEntry(const string entry) {
+    return false;
+}
+
+tuple<float, char> reinterpret_lat(const float lat) {
+    char n_or_s;
+    float re_lat;
+    // tuple<float, string> re_lat;
+    n_or_s = (lat > 0)?'N':'S';
+    re_lat = abs(lat);
+    return {re_lat, n_or_s};
+}
+
+tuple<float, char> reinterpret_lon(const float lon) {
+    char e_or_w;
+    float re_lon;
+    e_or_w = (lon < 0 || lon > 180)?'W':'E';
+    re_lon = abs(lon);
+    return {re_lon, e_or_w};
+}
+
+tuple<string, string> reinterpret_latlon(const float lat, const float lon) {
+    float re_lat, re_lon;
+    char n_or_s, e_or_w;
+    string str_lat, str_lon;
+    tie(re_lat, n_or_s) = reinterpret_lat(lat);
+    tie(re_lon, e_or_w) = reinterpret_lon(lon);
+    str_lat = to_string(re_lat) + "°" + n_or_s;
+    str_lon = to_string(re_lon) + "°" + e_or_w;    
+    return {str_lat, str_lon};
+}
+
+void printLunarCratersInfo(const lunar_crater crater) {
+    // const char cout_sep = '\n';
+    const string cout_sep = "\n\t";
+    string lat, lon;
+    tie(lat, lon) = reinterpret_latlon(crater.lat, crater.lon);
+    cout << "CRATER_ID: " << crater.crater_id << cout_sep
+         << "LAT_CIRC_IMG:        " << lat << cout_sep 
+         << "LON_CIRC_IMG:        " << lon << cout_sep 
+         << "DIAM_CIRC_IMG:       " << crater.diam << "km" << cout_sep 
+         << "DIAM_ELLI_ECCEN_IMG: " << crater.ecc << cout_sep 
+         << "DIAM_ELLI_ELLIP_IMG: " << crater.ell << cout_sep 
+         << "ARC_IMG:             " << crater.arc << endl;
+}
+
+bool readLunarCraterEntry(const string entry, const char sep) {
+    // lunar_crater crater;
+    string lat, lon, diam, ecc, n_pts;
+    stringstream str(entry);
+    string token;
+    uint count = 0;
+    while(getline(str, token, sep)) {
+        if(token.empty())
+            return false;
+        switch(count++) {
+            case CRATER_ID:
+            case LAT_CIRC_IMG:
+            case LON_CIRC_IMG:
+            case DIAM_CIRC_IMG:
+            case DIAM_ELLI_ECCEN_IMG:
+            case DIAM_ELLI_ELLIP_IMG:
+            case ARC_IMG:
+                cout<<token<<sep;
+            default:
+                break;
+        }
+    }
+    cout<<endl;
+    return true;
 }
